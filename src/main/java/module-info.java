725f0ab497d8aef57bf6346291bc52f3139ea247
Frank Delporte <f.delporte@televic.com>
2019-10-01 14:26:31 +0200

    Bump to version 0.0.3 + extra exports in module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,8 @@
-module be.webtechie {
+module be.webtechie.javafxlednumberdisplay {
     requires javafx.controls;
     exports be.webtechie.javafxlednumberdisplay;
+    exports be.webtechie.javafxlednumberdisplay.definition;
+    exports be.webtechie.javafxlednumberdisplay.component;
+    exports be.webtechie.javafxlednumberdisplay.segment;
+    exports be.webtechie.javafxlednumberdisplay.util;
 }
